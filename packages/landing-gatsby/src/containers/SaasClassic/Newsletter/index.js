a9ead6cc59d99801634ec53a570a9c9895bacfdf
Rafael González Vázquez <rafaelg@rafa.local>
2019-10-24 10:55:40 -0500

    added form

--- packages/landing-gatsby/src/containers/SaasClassic/Newsletter/index.js
+++ packages/landing-gatsby/src/containers/SaasClassic/Newsletter/index.js
@@ -14,22 +14,31 @@ function encode(data) {
     .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
     .join("&");}
 
-const Newsletter = ({
-  sectionWrapper,
-  textArea,
-  buttonArea,
-  buttonStyle,
-  title,
-  description,
-}) => {
+const Newsletter = ({ sectionWrapper, textArea, buttonArea, buttonStyle, title, description }) => {
+  
   const [formState, setFormState] = useState({
     email:""
   });
+
   const handelFormChange = (nombre, valor) => {
-    let newState = {...formState}
+    let newState = { ...formState };
     newState[nombre] = valor;
     setFormState(newState); 
   }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const body = encode({ "form-name": form.getAttribute("name"), ...formState });
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: body
+    }).then(() => console.log("Envio de formulario")).catch(error => console.log(error));
+  }
+
   return (
     <Box {...sectionWrapper} as="section">
       <Container>
@@ -43,42 +52,22 @@ const Newsletter = ({
           </Box>
           <Box {...buttonArea}>
             <ContactFormWrapper>
-              <form name="contact" onSubmit={(e) => {
-                const form=e.target;
-                fetch("/", {
-                  method: "POST",
-                  headers: { "Content-Type": "application/x-www-form-urlencoded" },
-                  body: encode({"form-name": form.getAttribute("name"),
-                  ...formState,
-                    })
-                  })
-                  .then(() => console.log("Envio de formulario"))
-                  .catch(error => console.log(error));
-                  e.preventDefault();
-              }}
-              style={{
-                width: "100%",
-                display: "flex"
-              }}>
-              <Input 
-                name="email"
-                inputType="email"
-                label="Email address"
-                iconPosition="right"
-                isMaterial={true}
-                className="email_input"
-                arial-label="email"
-                onChange={(valor) => handelFormChange("email",valor)}
-              />
-              <Button {...buttonStyle} title="ME INTERESA" type="submit"/>
+              <form name="contact" onSubmit={handleSubmit} data-netlify="true">
+                <Input name='email' inputType='email' label='Email address' iconPosition="right"
+                  isMaterial={true}
+                  className="email_input"
+                  arial-label="email"
+                  onChange={(valor) => handelFormChange("email",valor)}
+                />
+                <Button {...buttonStyle} title="ME INTERESA" type="submit"/>
               </form>
             </ContactFormWrapper>
           </Box>
         </NewsletterWrapper>
       </Container>
     </Box>
-  );
-};
+  )
+}
 
 Newsletter.propTypes = {
   sectionWrapper: PropTypes.object,
